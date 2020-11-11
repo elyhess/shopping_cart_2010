bb27b8025bbf516ff52ffe984a4cd984a106c795
Ely Hess <ely.hess@me.com>
2020-11-11 09:06:42 -0700

    Add vendor exists test

---
+++ test/vendor_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/item'
+require_relative '../lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    expected = {}
+    assert_equal expected, vendor.inventory
+  end
+
+
+end
